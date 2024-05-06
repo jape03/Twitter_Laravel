6c23b8d65a3fba9681b7e1a59ba97591c99a34d8
jape03 <japol33333@gmail.com>
2024-05-07 03:57:14 +0800

    first

---
+++ app/Http/Controllers/IdeaController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Idea;
+use Illuminate\Http\Request;
+
+class IdeaController extends Controller
+{
+    public function store(){
+
+        request()->validate(['idea' => 'required|min:3|max:240']);
+
+        $idea = Idea::create(["content" =>  request()->get('idea','	')]);
+
+        return redirect()->route('dashboard')->with('success','Idea was created successfully!');
+    }
+
+    public function destroy($id){
+
+        $idea = Idea::where('id', $id)->first();
+
+        $idea->delete();
+
+        return redirect()->route('dashboard')->with('success','Idea was deleted successfully!');
+
+    }
+
+}
